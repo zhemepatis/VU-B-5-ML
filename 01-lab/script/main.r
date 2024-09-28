0a7bac77119e9763c46f1a9dd1c72c55accc8b7d
zhemepatis <rink.gab@gmail.com>
2024-09-28 10:39:12 +0300

    added sampling

--- 01-lab/script/main.r
+++ 01-lab/script/main.r
@@ -5,3 +5,23 @@ ekg_data <- read.csv("data/ecg-data.csv", sep = ";", header = TRUE, na.strings=c
 str(ekg_data)
 ekg_data$RR_r_0 <- as.numeric(as.character(ekg_data$RR_r_0))
 ekg_data$RR_r_0.RR_r_1 <- as.numeric(as.character(ekg_data$RR_r_0.RR_r_1))
+
+# atsitiktinai pasirenkam duomenis
+sample_0 <- ekg_data[ekg_data$label == 0.0,]
+sample_0 <- sample_0[rowSums(is.na(sample_0)) != ncol(sample_0),]
+sample_0 <- sample_0[sample(1:nrow(sample_0), 1000),]
+
+sample_1 <- ekg_data[ekg_data$label == 1.0,]
+sample_1 <- sample_1[rowSums(is.na(sample_1)) != ncol(sample_1),]
+sample_1 <- sample_1[sample(1:nrow(sample_1), 1000),]
+
+sample_2 <- ekg_data[ekg_data$label == 2.0,]
+sample_2 <- sample_2[rowSums(is.na(sample_2)) != ncol(sample_2),]
+sample_2 <- sample_2[sample(1:nrow(sample_2), 1000),]
+
+# randam statistinius aprasus
+summary(sample_0)
+summary(sample_1)
+summary(sample_2)
+
+sample_binded <- rbind(sample_0, sample_1, sample_2)
