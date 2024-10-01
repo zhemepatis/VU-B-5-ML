95b5ec717a9eb9382354bda2ee19e4e06cd5d095
zhemepatis <rink.gab@gmail.com>
2024-10-01 06:47:13 +0300

    some cleanup

--- 01-lab/script/correlation.r
+++ 01-lab/script/correlation.r
@@ -1,18 +1,10 @@
 library(corrplot)
+source("script/functions/correlation.r")
 
 target_cols <- c("RR_l_0", "seq_size",  "signal_mean", "signal_std", "R_val", "T_val", "R_pos", "T_pos")
 col_num <- length(target_cols)
 
-correlation_table <- matrix(NA, ncol=col_num, nrow=col_num)
-colnames(correlation_table) <- target_cols
-rownames(correlation_table) <- target_cols
-
-# skaiciuojam koreliacija kiekvieno stulpelio su kiekvienu stulpeliu
-for (attr1 in target_cols) {
-  for (attr2 in target_cols) {
-    correlation_table[attr1, attr2] <- cor(ekg_data[[attr1]], ekg_data[[attr2]], method="pearson")
-  }
-}
-
-write.csv(correlation_table, "correlation_table.csv") # spausdinam koreliacijos lentelę
-corrplot(correlation_table, method="circle", type="lower", addCoef.col="black") # piesiam koreliacijos diagrama
+# koreliacija visiems duomenims bendrai
+correlation_table <- get_correlation_table(ekg_data, target_cols)
+write.csv(correlation_table, "correlation_table.csv")
+corrplot(correlation_table, method="circle", type="lower", addCoef.col="black")
