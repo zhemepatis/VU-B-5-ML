1f2e739e265bb7fdd40f460a8f58d7e7eb28a544
zhemepatis <rink.gab@gmail.com>
2024-10-03 19:03:46 +0300

    correlation using spearman's method

--- 01-lab/script/correlation.r
+++ 01-lab/script/correlation.r
@@ -1,14 +1,12 @@
 library(corrplot)
 source("script/functions/correlation.r")
 
-target_cols <- c("RR_l_0", "seq_size",  "signal_mean", "signal_std", "R_val", "T_val", "R_pos", "T_pos")
-col_num <- length(target_cols)
+target_cols <- c("RR_l_0", "seq_size",  "signal_mean", "signal_std", "R_val", "T_val", "R_pos", "T_pos", "label")
 
 # koreliacijos koeficientas visiems duomenims bendrai
 corr_coef_table <- get_correlation_coef_table(ekg_data, target_cols)
 write.csv(corr_coef_table, "corr_coef_table.csv")
 corrplot(corr_coef_table, method="circle", type="lower", addCoef.col="black")
 
-# koreliacijos p reiksme visiems duomenims bendrai
-corr_p_value_table <- get_correlation_p_value_table(ekg_data, target_cols)
-write.csv(corr_p_value_table, "corr_p_value_table.csv")
+p_value_table <- get_correlation_p_value_table(ekg_data, target_cols)
+write.csv(p_value_table, "p_value_table.csv")
--- 01-lab/script/functions/correlation.r
+++ 01-lab/script/functions/correlation.r
@@ -1,4 +1,6 @@
 get_correlation_coef_table <- function(data, target_cols) {
+  col_num <- length(target_cols)
+  
   coef_table <- matrix(NA, ncol=col_num, nrow=col_num)
   colnames(coef_table) <- target_cols
   rownames(coef_table) <- target_cols
@@ -6,7 +8,7 @@ get_correlation_coef_table <- function(data, target_cols) {
   # skaiciuojam koreliacijos koeficienta kiekvieno stulpelio su kiekvienu stulpeliu
   for (attr1 in target_cols) {
     for (attr2 in target_cols) {
-      result <- cor.test(ekg_data[[attr1]], ekg_data[[attr2]], method="pearson")
+      result <- cor.test(ekg_data[[attr1]], ekg_data[[attr2]], method="spearman", exact=FALSE)
       coef_table[attr1, attr2] <- result$estimate
     }
   }
@@ -15,6 +17,8 @@ get_correlation_coef_table <- function(data, target_cols) {
 }
 
 get_correlation_p_value_table <- function(data, target_cols) {
+  col_num <- length(target_cols)
+  
   p_value_table <- matrix(NA, ncol=col_num, nrow=col_num)
   colnames(p_value_table) <- target_cols
   rownames(p_value_table) <- target_cols
@@ -22,8 +26,8 @@ get_correlation_p_value_table <- function(data, target_cols) {
   # skaiciuojam koreliacija p reiksme stulpelio su kiekvienu stulpeliu
   for (attr1 in target_cols) {
     for (attr2 in target_cols) {
-      result <- cor.test(ekg_data[[attr1]], ekg_data[[attr2]], method="pearson")
-      p_value_table[attr1, attr2] <- result$estimate
+      result <- cor.test(ekg_data[[attr1]], ekg_data[[attr2]], method="spearman", exact=FALSE)
+      p_value_table[attr1, attr2] <- result$p.value
     }
   }
   
