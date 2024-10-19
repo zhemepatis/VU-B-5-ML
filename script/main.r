117e41bfbeecccd5ecf4aa7bd9a9a91eb0492c0d
zhemepatis <rink.gab@gmail.com>
2024-10-19 11:56:05 +0300

    folder refactoring

--- script/main.r
+++ script/main.r
@@ -2,7 +2,7 @@ ekg_data <-
   read.csv("data/ecg-data.csv", sep = ";", header = TRUE, na.strings = c(""))
 
 # data preparation
-source("script/type-conversion.r")
-source("script/sampling.r")
-source("script/data-fillup.r")
-source("script/feature-selection.r")
+source("script/data-preparation/type-conversion.r")
+source("script/data-preparation/sampling.r")
+source("script/data-preparation/data-fillup.r")
+source("script/data-preparation/feature-selection.r")
