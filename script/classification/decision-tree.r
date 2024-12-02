2160f10cc1b8f3d2590e88f5127a89ccaa8c4cfe
zhemepatis <rink.gab@gmail.com>
2024-12-02 07:16:45 +0200

    minor changes

--- script/classification/decision-tree.r
+++ script/classification/decision-tree.r
@@ -3,28 +3,28 @@ library(rpart)
 library(ggplot2)
 source("script/analysis/prediction_plot.r")
 
-# Ensure label columns are factors
 training_set$label <- as.factor(training_set$label)
 validation_set$label <- as.factor(validation_set$label)
 
-# Train the decision tree model
+# apmokom modeli naudojant pilna duomenu aibe
 model <- rpart(label ~ ., data = training_set)
 rpart.plot(model, type = 1, extra = "auto")
 
-# Make predictions on the validation set
+# validuojam modeli
 target_cols <- setdiff(names(validation_set), "label")
 prediction <- predict(model, newdata = validation_set[, target_cols], type = 'class')
 
-# Confusion matrix
+# skaiciuojam sumaisymo matrica
 confusion_matrix <- table(validation_set$label, prediction)
 print(confusion_matrix)
 
-# Perform UMAP dimensionality reduction
+# sumazinam dimensijas vizualizavimui
 training_set_reduced <- perform_umap(training_set)
 validation_set_reduced <- perform_umap(validation_set)
 
-# Add predictions and correctness information
+# pridedam spejimus
 validation_set_reduced$prediction <- prediction
 validation_set_reduced$correct <- validation_set_reduced$label == prediction
 
+# vizualizuojam rezultatus
 plot_predictions(validation_set_reduced, prediction)
