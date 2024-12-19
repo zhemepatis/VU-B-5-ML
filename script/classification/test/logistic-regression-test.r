93d370f4a029aeeef13b3076c0fbf2bb9b5221b2
zhemepatis <rink.gab@gmail.com>
2024-12-19 10:27:35 +0200

    added roc

--- script/classification/test/logistic-regression-test.r
+++ script/classification/test/logistic-regression-test.r
@@ -8,6 +8,24 @@ source("script/classification/logistic-regression.r")
 source("script/data-preparation/norm.r")
 source("script/dimension-reduction/umap.r")
 
+
+roc_curve <- function(data, prediction_prob, positive_class = "1", title = "ROC kreivė") {
+  data$label <- factor(data$label, levels = colnames(prediction_prob))
+  roc_obj <- roc(data$label, prediction_prob[, positive_class], levels = rev(levels(data$label)))
+  
+  print(
+    ggroc(roc_obj) +
+      ggtitle(title) +
+      xlab("1 - Specificity (False Positive Rate)") +
+      ylab("Sensitivity (True Positive Rate)") +
+      theme_minimal() +
+      geom_abline(linetype = "dashed", color = "gray")
+  )
+  
+  auc <- auc(roc_obj)
+  return(auc)
+}
+
 # normuojam duomenis
 normalization_result <- normalize_sets(training_set, test_set)
 training_set <- normalization_result$main_set
@@ -18,24 +36,26 @@ training_set_2d <- training_set[, target_cols]
 test_set_2d <- test_set[, target_cols]
 
 # nesuspausta, pilna duomenu aibe
-results <- apply_logistic_regression(training_set, test_set, threshold = 0.5, maxit = 1000, epsilon = 1e-8)
+lr_results <- apply_logistic_regression(training_set, test_set, threshold = 0.5, maxit = 1000, epsilon = 1e-8)
 prediction <- results$prediction
 prediction_prob <- results$prediction_prob
 
 test_set_reduced <- perform_umap(test_set, set_seed = TRUE)
 
 plot_predictions(test_set_reduced, prediction, "Logistinės regresijos klasifikavimo rezultatai pilnai aibei")
-auc <- roc_curve(test_set, prediction_prob, positive_class = "2", "Logistinės regresijos ROC kreivė pilnai aibei")
-print(auc)
+
+
+
+
 
 # apirbota, suspausta duomenu aibe
-training_set_2d <- perform_umap(training_set_2d, set_seed = TRUE)
-test_set_2d <- perform_umap(test_set_2d, set_seed = TRUE)
-
-results <- apply_logistic_regression(training_set_2d, test_set_2d, threshold = 0.5, maxit = 1000, epsilon = 1e-8)
-prediction <- results$prediction
-prediction_prob <- results$prediction_prob
-
-plot_predictions(test_set_2d, prediction, "Logistinės regresijos klasifikavimo rezultatai apribotai suspaustai aibei")
-auc <- roc_curve(test_set_2d, prediction_prob, positive_class = "2", "Logstinės regresijos ROC kreivė apribotai suspaustai aibei")
-print(auc)
+# training_set_2d <- perform_umap(training_set_2d, set_seed = TRUE)
+# test_set_2d <- perform_umap(test_set_2d, set_seed = TRUE)
+# 
+# results <- apply_logistic_regression(training_set_2d, test_set_2d, threshold = 0.5, maxit = 1000, epsilon = 1e-8)
+# prediction <- results$prediction
+# prediction_prob <- results$prediction_prob
+# 
+# plot_predictions(test_set_2d, prediction, "Logistinės regresijos klasifikavimo rezultatai apribotai suspaustai aibei")
+# auc <- roc_curve(test_set_2d, prediction_prob, positive_class = "2", "Logstinės regresijos ROC kreivė apribotai suspaustai aibei")
+# print(auc)
